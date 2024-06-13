9deae9813591d9d40400ca36b8c5ebb01bfce088
huaisha1224 <sam.hxq@gmail.com>
2024-06-13 13:36:05 +0800

    提交油猴JS脚本
    提交油猴JS脚本

---
+++ tampermonkey/MobileRewards.user.js
@@ -0,0 +1,162 @@
+// ==UserScript==
+// @name         移动端微软Rewards每日任务脚本
+// @version      2024.6.7
+// @description  超市卡，加油卡，苹果卡，电影卡通通都有。
+// @author       怀沙2049
+// @match        https://www.bing.com/*
+// @match        https://cn.bing.com/*
+// @license      GNU GPLv3
+// @icon         https://www.bing.com/favicon.ico
+// @connect      tenapi.cn
+// @run-at       document-end
+// @note         更新于 2024年6月7日
+// @supportURL   https://greasyfork.org/zh-CN/users/1192640-huaisha1224
+// @homepageURL  https://greasyfork.org/zh-CN/users/1192640-huaisha1224
+// @grant        GM_registerMenuCommand
+// @grant        GM_addStyle
+// @grant        GM_openInTab
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @namespace    https://greasyfork.org/zh-CN/users/1192640-huaisha1224
+// ==/UserScript==
+
+var max_rewards = 30; //重复执行的次数
+//每执行4次搜索后插入暂停时间,解决账号被监控不增加积分的问题
+var pause_time = 6; // 暂停时长建议为10分钟（600000毫秒=10分钟）
+var search_words = []; //搜索词
+
+var default_search_words = ["盛年不重来，一日难再晨", "千里之行，始于足下", "少年易学老难成，一寸光阴不可轻", "敏而好学，不耻下问", "海内存知已，天涯若比邻", "三人行，必有我师焉",
+    "莫愁前路无知已，天下谁人不识君", "人生贵相知，何用金与钱", "天生我材必有用", "海纳百川有容乃大；壁立千仞无欲则刚", "穷则独善其身，达则兼济天下", "读书破万卷，下笔如有神",
+    "学而不思则罔，思而不学则殆", "一年之计在于春，一日之计在于晨", "莫等闲，白了少年头，空悲切", "少壮不努力，老大徒伤悲", "一寸光阴一寸金，寸金难买寸光阴", "近朱者赤，近墨者黑",
+    "吾生也有涯，而知也无涯", "纸上得来终觉浅，绝知此事要躬行", "学无止境", "己所不欲，勿施于人", "天将降大任于斯人也", "鞠躬尽瘁，死而后已", "书到用时方恨少", "天下兴亡，匹夫有责",
+    "人无远虑，必有近忧", "为中华之崛起而读书", "一日无书，百事荒废", "岂能尽如人意，但求无愧我心", "人生自古谁无死，留取丹心照汗青", "吾生也有涯，而知也无涯", "生于忧患，死于安乐"]
+
+
+//{weibohot}微博热搜榜//{douyinhot}抖音热搜榜/{zhihuhot}知乎热搜榜/{baiduhot}百度热搜榜/{toutiaohot}今日头条热搜榜/
+var keywords_source = ['douyinhot','zhihuhot','baiduhot','toutiaohot'];
+var random_keywords_source = keywords_source[Math.floor(Math.random() * keywords_source.length)]
+var current_source_index = 0; // 当前搜索词来源的索引
+
+/**
+ * 尝试从多个搜索词来源获取搜索词，如果所有来源都失败，则返回默认搜索词。
+ * @returns {Promise<string[]>} 返回搜索到的name属性值列表或默认搜索词列表
+ */
+async function douyinhot_dic() {
+    while (current_source_index < keywords_source.length) {
+        const source = keywords_source[current_source_index]; // 获取当前搜索词来源
+        try {
+            const response = await fetch("https://tenapi.cn/v2/" + source); // 发起网络请求
+            if (!response.ok) {
+                throw new Error('HTTP error! status: ' + response.status); // 如果响应状态不是OK，则抛出错误
+            }
+            const data = await response.json(); // 解析响应内容为JSON
+            
+            if (data.data.some(item => item)) {
+                // 如果数据中存在有效项
+                // 提取每个元素的name属性值
+                const names = data.data.map(item => item.name);
+                return names; // 返回搜索到的name属性值列表
+            }
+        } catch (error) {
+            // 当前来源请求失败，记录错误并尝试下一个来源
+            console.error('搜索词来源请求失败:', error);
+        }
+        
+        // 尝试下一个搜索词来源
+        current_source_index++;
+    }
+    
+    // 所有搜索词来源都已尝试且失败
+    console.error('所有搜索词来源请求失败');
+    return default_search_words; // 返回默认搜索词列表
+}
+// 调用douyinhot_dic函数，获取names列表
+douyinhot_dic()
+    .then(names => {
+        //   console.log(names[0]);
+        search_words = names;
+        exec()
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+// 定义菜单命令：开始
+let menu1 = GM_registerMenuCommand('开始', function () {
+    GM_setValue('Cnt', 0); // 将计数器重置为0
+    location.href = "https://www.bing.com/?br_msg=Please-Wait"; // 跳转到Bing首页
+}, 'o');
+
+// 定义菜单命令：停止
+let menu2 = GM_registerMenuCommand('停止', function () {
+    GM_setValue('Cnt', max_rewards + 10); // 将计数器设置为超过最大搜索次数，以停止搜索
+}, 'o');
+
+
+// 生成指定长度的包含大写字母、数字的随机字符串
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        // 从字符集中随机选择字符，并拼接到结果字符串中
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function exec() {
+    // 生成随机延迟时间
+    let randomDelay = Math.floor(Math.random() * 20000) + 10000; // 10000 毫秒 = 10 秒
+    let randomString = generateRandomString(4); //生成4个长度的随机字符串
+    let randomCvid = generateRandomString(32); //生成32位长度的cvid
+    'use strict';
+
+    // 检查计数器的值，若为空则设置为超过最大搜索次数
+    if (GM_getValue('Cnt') == null) {
+        GM_setValue('Cnt', max_rewards + 10);
+    }
+
+    // 获取当前搜索次数
+    let currentSearchCount = GM_getValue('Cnt');
+
+    // 根据计数器的值选择搜索引擎
+    if (currentSearchCount <= max_rewards / 2) {
+        let tt = document.getElementsByTagName("title")[0];
+        tt.innerHTML = "[" + currentSearchCount + " / " + max_rewards + "] " + tt.innerHTML; // 在标题中显示当前搜索次数
+
+        setTimeout(function () {
+            GM_setValue('Cnt', currentSearchCount + 1); // 将计数器加1
+            let nowtxt = search_words[currentSearchCount]; // 获取当前搜索词
+
+            // 检查是否需要暂停
+            if ((currentSearchCount + 1) % 5 === 0) {
+                // 暂停指定时长
+                setTimeout(function() {
+                    location.href = "https://www.bing.com/search?q=" + encodeURI(nowtxt) + "&form=" + randomString + "&cvid=" + randomCvid; // 在Bing搜索引擎中搜索
+                }, pause_time);
+            } else {
+                location.href = "https://www.bing.com/search?q=" + encodeURI(nowtxt) + "&form=" + randomString + "&cvid=" + randomCvid; // 在Bing搜索引擎中搜索
+            }
+        }, randomDelay);
+    } else if (currentSearchCount > max_rewards / 2 && currentSearchCount < max_rewards) {
+        let tt = document.getElementsByTagName("title")[0];
+        tt.innerHTML = "[" + currentSearchCount + " / " + max_rewards + "] " + tt.innerHTML; // 在标题中显示当前搜索次数
+
+        setTimeout(function () {
+            GM_setValue('Cnt', currentSearchCount + 1); // 将计数器加1
+            let nowtxt = search_words[currentSearchCount]; // 获取当前搜索词
+
+            // 检查是否需要暂停
+            if ((currentSearchCount + 1) % 5 === 0) {
+                // 暂停指定时长
+                setTimeout(function() {
+                    location.href = "https://cn.bing.com/search?q=" + encodeURI(nowtxt) + "&form=" + randomString + "&cvid=" + randomCvid; // 在Bing搜索引擎中搜索
+                }, pause_time);
+            } else {
+                location.href = "https://cn.bing.com/search?q=" + encodeURI(nowtxt) + "&form=" + randomString + "&cvid=" + randomCvid; // 在Bing搜索引擎中搜索
+            }
+        }, randomDelay);
+    }
+}
