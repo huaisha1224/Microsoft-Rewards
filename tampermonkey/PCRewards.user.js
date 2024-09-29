9174c6204aa2f88738c6d1936f9f7d3d3ae6f20c
huaisha1224 <sam.hxq@gmail.com>
2024-09-29 16:31:09 +0800

    添加滚动到页面底部的功能
    每次访问页面之后滚动页面到底部

--- tampermonkey/PCRewards.user.js
+++ tampermonkey/PCRewards.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Microsoft Bing Rewards每日任务脚本
-// @version      V3.0.2
+// @version      V3.0.3
 // @description  自动完成微软Rewards每日搜索任务,每次运行时获取抖音/微博/哔哩哔哩/百度/头条热门词,避免使用同样的搜索词被封号。
-// @note         更新于 2024年9月21日
+// @note         更新于 2024年9月29日
 // @author       怀沙2049
 // @match        https://*.bing.com/*
 // @license      GNU GPLv3
@@ -20,18 +20,18 @@
 
 var max_rewards = 40; //重复执行的次数
 //每执行4次搜索后插入暂停时间,解决账号被监控不增加积分的问题
-var pause_time = 9; // 暂停时长建议为16分钟,也就是960000（60000毫秒=1分钟）
+var pause_time = 9; // 暂停时长建议为16分钟,也就是960000(60000毫秒=1分钟)
 var search_words = []; //搜索词
 
 //默认搜索词，热门搜索词请求失败时使用
-var default_search_words = ["盛年不重来，一日难再晨", "千里之行，始于足下", "少年易学老难成，一寸光阴不可轻", "敏而好学，不耻下问", "海内存知已，天涯若比邻","三人行，必有我师焉",
+var default_search_words = ["盛年不重来，一日难再晨", "千里之行，始于足下", "少年易学老难成，一寸光阴不可轻", "敏而好学，不耻下问", "海内存知已，天涯若比邻", "三人行，必有我师焉",
     "莫愁前路无知已，天下谁人不识君", "人生贵相知，何用金与钱", "天生我材必有用", "海纳百川有容乃大；壁立千仞无欲则刚", "穷则独善其身，达则兼济天下", "读书破万卷，下笔如有神",
-    "学而不思则罔，思而不学则殆", "一年之计在于春，一日之计在于晨", "莫等闲，白了少年头，空悲切", "少壮不努力，老大徒伤悲", "一寸光阴一寸金，寸金难买寸光阴","近朱者赤，近墨者黑",
-    "吾生也有涯，而知也无涯", "纸上得来终觉浅，绝知此事要躬行", "学无止境", "己所不欲，勿施于人", "天将降大任于斯人也", "鞠躬尽瘁，死而后已", "书到用时方恨少","天下兴亡，匹夫有责",
-    "人无远虑，必有近忧","为中华之崛起而读书","一日无书，百事荒废","岂能尽如人意，但求无愧我心","人生自古谁无死，留取丹心照汗青","吾生也有涯，而知也无涯","生于忧患，死于安乐",
-    "言必信，行必果","读书破万卷，下笔如有神","夫君子之行，静以修身，俭以养德","老骥伏枥，志在千里","一日不读书，胸臆无佳想","王侯将相宁有种乎","淡泊以明志。宁静而致远,","卧龙跃马终黄土"]
+    "学而不思则罔，思而不学则殆", "一年之计在于春，一日之计在于晨", "莫等闲，白了少年头，空悲切", "少壮不努力，老大徒伤悲", "一寸光阴一寸金，寸金难买寸光阴", "近朱者赤，近墨者黑",
+    "吾生也有涯，而知也无涯", "纸上得来终觉浅，绝知此事要躬行", "学无止境", "己所不欲，勿施于人", "天将降大任于斯人也", "鞠躬尽瘁，死而后已", "书到用时方恨少", "天下兴亡，匹夫有责",
+    "人无远虑，必有近忧", "为中华之崛起而读书", "一日无书，百事荒废", "岂能尽如人意，但求无愧我心", "人生自古谁无死，留取丹心照汗青", "吾生也有涯，而知也无涯", "生于忧患，死于安乐",
+    "言必信，行必果", "读书破万卷，下笔如有神", "夫君子之行，静以修身，俭以养德", "老骥伏枥，志在千里", "一日不读书，胸臆无佳想", "王侯将相宁有种乎", "淡泊以明志。宁静而致远,", "卧龙跃马终黄土"]
 //{weibohot}微博热搜榜//{douyinhot}抖音热搜榜/{zhihuhot}知乎热搜榜/{baiduhot}百度热搜榜/{toutiaohot}今日头条热搜榜/
-var keywords_source = ['BaiduHot','TouTiaoHot','DouYinHot', 'WeiBoHot'];
+var keywords_source = ['BaiduHot', 'TouTiaoHot', 'DouYinHot', 'WeiBoHot'];
 var random_keywords_source = keywords_source[Math.floor(Math.random() * keywords_source.length)]
 var current_source_index = 0; // 当前搜索词来源的索引
 
@@ -48,7 +48,7 @@ async function douyinhot_dic() {
                 throw new Error('HTTP error! status: ' + response.status); // 如果响应状态不是OK，则抛出错误
             }
             const data = await response.json(); // 解析响应内容为JSON
-            
+
             if (data.data.some(item => item)) {
                 // 如果数据中存在有效项
                 // 提取每个元素的title属性值
@@ -59,11 +59,11 @@ async function douyinhot_dic() {
             // 当前来源请求失败，记录错误并尝试下一个来源
             console.error('搜索词来源请求失败:', error);
         }
-        
+
         // 尝试下一个搜索词来源
         current_source_index++;
     }
-    
+
     // 所有搜索词来源都已尝试且失败
     console.error('所有搜索词来源请求失败');
     return default_search_words; // 返回默认搜索词列表
@@ -123,7 +123,7 @@ function generateRandomString(length) {
 
 function exec() {
     // 生成随机延迟时间
-    let randomDelay = Math.floor(Math.random() * 20000) + 10000; // 10000 毫秒 = 10 秒
+    let randomDelay = Math.floor(Math.random() * 20000) + 10000; // 生成10秒到30秒之间的随机数
     let randomString = generateRandomString(4); //生成4个长度的随机字符串
     let randomCvid = generateRandomString(32); //生成32位长度的cvid
     'use strict';
@@ -135,7 +135,7 @@ function exec() {
 
     // 获取当前搜索次数
     let currentSearchCount = GM_getValue('Cnt');
-
+    smoothScrollToBottom(); // 添加执行滚动页面到底部的操作
     // 根据计数器的值选择搜索引擎
     if (currentSearchCount <= max_rewards / 2) {
         let tt = document.getElementsByTagName("title")[0];
@@ -145,11 +145,9 @@ function exec() {
         setTimeout(function () {
             let nowtxt = search_words[currentSearchCount]; // 获取当前搜索词
             nowtxt = AutoStrTrans(nowtxt); // 对搜索词进行替换
-
             // 检查是否需要暂停
             if ((currentSearchCount + 1) % 5 === 0) {
-                // 暂停指定时长
-                setTimeout(function() {
+                setTimeout(function () {
                     location.href = "https://www.bing.com/search?q=" + encodeURI(nowtxt) + "&form=" + randomString + "&cvid=" + randomCvid; // 在Bing搜索引擎中搜索
                 }, pause_time);
             } else {
@@ -164,11 +162,9 @@ function exec() {
         setTimeout(function () {
             let nowtxt = search_words[currentSearchCount]; // 获取当前搜索词
             nowtxt = AutoStrTrans(nowtxt); // 对搜索词进行替换
-
             // 检查是否需要暂停
             if ((currentSearchCount + 1) % 5 === 0) {
-                // 暂停指定时长
-                setTimeout(function() {
+                setTimeout(function () {
                     location.href = "https://cn.bing.com/search?q=" + encodeURI(nowtxt) + "&form=" + randomString + "&cvid=" + randomCvid; // 在Bing搜索引擎中搜索
                 }, pause_time);
             } else {
@@ -176,4 +172,8 @@ function exec() {
             }
         }, randomDelay);
     }
+    // 实现平滑滚动到页面底部的函数
+    function smoothScrollToBottom() {
+         document.documentElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        }
 }
